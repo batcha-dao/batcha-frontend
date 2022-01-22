7dff354b2e235210f636200d2eb89177771d4ca1
clovermini22 <clovermini22@gmail.com>
2022-01-22 20:03:30 +0900

    Import CORS

--- frontend/server.js
+++ frontend/server.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
+var cors = require('cors');
 
 const PORT = 3000;
 
 app.use(express.static('public'))
-
+app.use(cors())
 app.get('/', function(req,res) {
     res.sendFile(__dirname + "/index.html")
 });
